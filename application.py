ba7ca2f349a6d83c44b79972ce07e60ec53733c1
Kee Zheng Khai <zhengkhai2010@hotmail.com>
2019-06-12 11:13:04 +0800

    enable single file donwload

--- application.py
+++ application.py
@@ -8,11 +8,24 @@
 
 
 from flask import Flask, render_template, request, redirect, url_for
-import os, json, boto3
+import os, json, boto3, botocore
+
 
 app = Flask(__name__)
 
 
+# Load necessary information into the application
+S3_BUCKET = os.environ.get('S3_BUCKET')
+S3_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+s3 = boto3.client(
+  's3',
+  aws_access_key_id=S3_ACCESS_KEY_ID,
+  aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
+
+relative_path='./images'
+abs_path=os.path.abspath(relative_path)
 
 # Listen for GET requests to yourdomain.com/account/
 @app.route("/account/")
@@ -24,60 +37,48 @@ def account():
 # Listen for POST requests to yourdomain.com/submit_form/
 @app.route("/submit-form/", methods = ["POST"])
 def submit_form():
-  # Collect the data posted from the HTML form in account.html:
-  username = request.form["username"]
-  full_name = request.form["full-name"]
-  avatar_url = request.form["avatar-url"]
-
-  # Provide some procedure for storing the new details
-  # update_account(username, full_name, avatar_url)
-
-  # Redirect to the user's profile page, if appropriate
   return render_template('profile.html')
 
+@app.route('/new/')
+def show():
+  keys=[]
+  resp=s3.list_objects_v2(Bucket=S3_BUCKET)
+  for obj in resp['Contents']:
+    keys.append(obj['Key'])
+  return render_template('new.html',keys=keys)
+
+@app.route('/download/<object>')
+def download(object):
+  file=os.getcwd()+"\\"+object
+  service=boto3.resource('s3')
+  try:
+    r=service.Bucket(S3_BUCKET).download_file(object, file)
+  except botocore.exceptions.Client as e:
+    if e.response['Error']['Code'] == "404":
+      alert("The object does not exist.")
+    else:
+      raise
+  return redirect(url_for('account'))
 
 
-# Listen for GET requests to yourdomain.com/sign_s3/
-#
-# Please see https://gist.github.com/RyanBalfanz/f07d827a4818fda0db81 for an example using
-# Python 3 for this view.
-@app.route("/upload")
-def gogo():
-  file=request.args.get('filename')
 
 @app.route('/sign-s3/')
 def sign_s3():
-  # Load necessary information into the application
-  S3_BUCKET = os.environ.get('S3_BUCKET')
-  S3_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
-  AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
-  
-
-  # Load required data from the request
   file_name = request.args.get('file-name')
   file_type = request.args.get('file-type')
-  # Initialise the S3 client
-  s3 = boto3.client(
-    's3',
-    aws_access_key_id=S3_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-  )
 
-  # Generate and return the presigned URL
   presigned_post = s3.generate_presigned_post(
     Bucket = S3_BUCKET,
     Key = file_name,
     ExpiresIn = 3600
   )
 
-  # Return the data to the client
   return json.dumps({
     'data': presigned_post,
     'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
   })
 
 
-# Main code
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port = port)
