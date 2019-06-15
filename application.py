8a93f243a872fa196f73e9203ae96fa49c891e9e
Kee Zheng Khai <zhengkhai2010@hotmail.com>
2019-06-16 01:23:22 +0800

    enable multiple file download and fast download

--- application.py
+++ application.py
@@ -11,13 +11,16 @@ from flask import Flask, render_template, request, redirect, url_for
 import os, json, boto3, botocore
 from boto3.s3.transfer import S3Transfer
 from botocore.exceptions import ClientError
-
-
+from flask_util_js import FlaskUtilJs
+import threading, sys
+from boto3.s3.transfer import TransferConfig
 
 app = Flask(__name__)
+fujs=FlaskUtilJs(app)
 
-
-# Load necessary information into the application
+@app.context_processor
+def inject_fujs():
+  return dict(fujs=fujs)
 
 client=boto3.client('s3','ap-southeast-1')
 transfer =S3Transfer(client)
@@ -30,17 +33,12 @@ s3 = boto3.client(
   aws_secret_access_key=AWS_SECRET_ACCESS_KEY
 )
 
-relative_path='./images'
-abs_path=os.path.abspath(relative_path)
 
-# Listen for GET requests to yourdomain.com/account/
 @app.route("/account/")
 def account():
   # receive file, then pass it on
   return render_template('account.html')
 
-
-# Listen for POST requests to yourdomain.com/submit_form/
 @app.route("/submit-form/", methods = ["POST"])
 def submit_form():
   return render_template('profile.html')
@@ -53,19 +51,22 @@ def show():
     keys.append(obj['Key'])
   return render_template('new.html',keys=keys)
 
-@app.route('/download/<object>')
-def download(object):
+@app.route('/download/')
+def download():
+  object=request.args.get('object')
   service=boto3.resource('s3')
-  file=os.getcwd()+"\\"+object
+  path=os.getcwd()+"\\"+object
   try:
-    progress=ProgressPercentage(transfer._manager._client,S3_BUCKET, r'file')
-    transfer.download_file(S3_BUCKET,object, r'file', callback=progress)
+    # progress=ProgressPercentage(transfer._manager._client, S3_BUCKET, path, data=object)
+    transfer.download_file(S3_BUCKET, object, path)
   except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == "404":
       alert("The object does not exist.")
     else:
       raise
-  return redirect(url_for('account'))
+  return json.dumps({
+    'data': 'successfully downloaded files!'
+  })
 
 
 
@@ -74,10 +75,13 @@ def sign_s3():
   file_name = request.args.get('file-name')
   file_type = request.args.get('file-type')
 
+  GB=1024**3
+  # config=TransferConfig(multipart_threshold=5*GB)
   presigned_post = s3.generate_presigned_post(
     Bucket = S3_BUCKET,
     Key = file_name,
-    ExpiresIn = 3600
+    ExpiresIn = 3600,
+    # Config=config
   )
 
   return json.dumps({
@@ -85,21 +89,21 @@ def sign_s3():
     'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
   })
 
+# WIP - download progress bar
+# class ProgressPercentage(object):
+#   def __init__(self, client, bucket, filename, data):
+#     self._filename=filename
+#     self._size=float(os.path.getsize(filename))
+#     self._seen_so_far =0
+#     self._lock = threading.Lock()
+#     self._size=client.head_object(Bucket=S3_BUCKET, Key=data)['ContentLength']
 
-class ProgressPercentage(object):
-  def __init__(self, client, bucket, filename):
-    self._filename=filename
-    self._size=float(os.path.getsize(filename))
-    self._seen_so_far =0
-    self._lock = threading.Lock()
-    self._size=client.head_object(Bucket=S3_BUCKET, key=filename).ContentLength
-
-  def __call__(self, bytes_amount):
-    with self._lock:
-      self._seen_so_far+=bytes_amount
-      percentage=(self._seen_so_far/self.size)*100
-      LoggingFile('{} is the filename, {} out of {} done. The percentage completed is {} %'.format(str(self._filename), str(self._seen_so_far), str(self._size), str(percentage)))
-      sys.stdout.flush()
+#   def __call__(self, bytes_amount):
+#     with self._lock:
+#       self._seen_so_far+=bytes_amount
+#       percentage=(self._seen_so_far/self._size)*100
+#       # LoggingFile('{} is the filename, {} out of {} done. The percentage completed is {} %'.format(str(self._filename), str(self._seen_so_far), str(self._size), str(percentage)))
+#       sys.stdout.flush()
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
